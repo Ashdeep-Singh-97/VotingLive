459bc8dc09e755797c34186489b667f03e7ef342
Ashdeep-Singh-97 <ashdeepsingh1997@gmail.com>
2024-09-19 01:29:11 +0530

    Components added.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import Dummy from "./dummy"
 import './App.css'
 import Web3Provider from "./context/web3Provider"
+import { RouterProvider } from "react-router-dom"
+import { routes } from "./routes/routes";
 
 function App() {
-
   return (
     <>
       <Web3Provider>
-        <Dummy />
+        <RouterProvider router={routes}></RouterProvider>
       </Web3Provider>
     </>
   )
---
+++ src/components/Wallet/Wallet.jsx
@@ -0,0 +1,9 @@
+const Wallet = () => {
+    return(
+        <h1>
+            Wallet Component is here
+        </h1>
+    )
+}
+
+export default Wallet;
